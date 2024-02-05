7ced64058b6f414acb03cdc49b6170bdf513e431
d33bs <dave.bunten@cuanschutz.edu>
2024-02-05 15:33:20 -0700

    update for kz data ingest

--- src/notebooks/rtx_kg2_sample_parquet_to_kuzu.py
+++ src/notebooks/rtx_kg2_sample_parquet_to_kuzu.py
@@ -58,15 +58,16 @@ pathlib.Path(kuzu_dir).mkdir(exist_ok=True)
 db = kuzu.Database(f"{kuzu_dir}")
 kz_conn = kuzu.Connection(db)
 
-type(kz_conn)
-
 
 def generate_cypher_table_create_stmt_from_parquet_file(
     parquet_file: str,
     table_type: Literal["node", "rel"],
     table_name: str,
     table_pkey_parquet_field_name: str = "id",
-    rel_table_field_mapping: Dict[str, str] = {"from": "Nodes", "to": "Nodes"},
+    rel_table_field_mapping: Dict[str, str] = {"from": "nodes", "to": "nodes"},
+    # specify a map for from to specification
+    # to move these to first two cols of related table
+    edges_to_or_from_fieldnames: List[str] = ["subject", "object"],
 ):
 
     parquet_schema = parquet.read_schema(parquet_file)
@@ -77,6 +78,7 @@ def generate_cypher_table_create_stmt_from_parquet_file(
         )
 
     # Map Parquet data types to Cypher data types
+    # more details here: https://kuzudb.com/docusaurus/cypher/data-types/
     parquet_to_cypher_type_mapping = {
         "string": "STRING",
         "int32": "INT32",
@@ -86,8 +88,8 @@ def generate_cypher_table_create_stmt_from_parquet_file(
         "double": "FLOAT",
         "boolean": "BOOLEAN",
         "object": "MAP",
-        "array": "LIST",
-        "list<element: string>": "INT64[]",
+        "array": "INT64[]",
+        "list<element: string>": "STRING[]",
         "null": "NULL",
         "date": "DATE",
         "time": "TIME",
@@ -102,7 +104,8 @@ def generate_cypher_table_create_stmt_from_parquet_file(
             # note: we use string splitting here for nested types
             # for ex. list<element: string>
             f"{field.name} {parquet_to_cypher_type_mapping.get(str(field.type))}"
-            for field in parquet_schema
+            for idx, field in enumerate(parquet_schema)
+            if table_type == "node" or (table_type == "rel" and idx > 1)
         ]
     )
 
@@ -131,32 +134,32 @@ def drop_table_if_exists(kz_conn: kuzu.connection.Connection, table_name: str):
 
 
 # +
-drop_table_if_exists(kz_conn=kz_conn, table_name="Relationships")
-drop_table_if_exists(kz_conn=kz_conn, table_name="Nodes")
+dataset_name_to_cypher_table_type_map = {"nodes": "node", "edges": "rel"}
 
+drop_table_if_exists(kz_conn=kz_conn, table_name="edges")
+drop_table_if_exists(kz_conn=kz_conn, table_name="nodes")
 
-for path in pathlib.Path(parquet_dir).glob("*"):
+# note: we provide specific ordering here to ensure nodes are created before edges
+for path in [f"{parquet_dir}/nodes", f"{parquet_dir}/edges"]:
 
     # use first file discovered as basis for schema
     first_pq_file = next(pathlib.Path(path).glob("*.parquet"))
 
-    if first_pq_file.parent.name == "nodes":
-
-        create_stmt = generate_cypher_table_create_stmt_from_parquet_file(
-            parquet_file=first_pq_file,
-            table_type="node",
-            table_name="Nodes",
-            table_pkey_parquet_field_name="id",
-        )
-
-    elif first_pq_file.parent.name == "edges":
-
-        create_stmt = generate_cypher_table_create_stmt_from_parquet_file(
-            parquet_file=first_pq_file,
-            table_type="rel",
-            table_name="Relationships",
-            table_pkey_parquet_field_name="id",
-        )
+    create_stmt = generate_cypher_table_create_stmt_from_parquet_file(
+        parquet_file=first_pq_file,
+        table_type=dataset_name_to_cypher_table_type_map[first_pq_file.parent.name],
+        table_name=first_pq_file.parent.name,
+        table_pkey_parquet_field_name="id",
+    )
 
     print(f"Using the following create statement to create table:\n\n{create_stmt}\n\n")
     kz_conn.execute(create_stmt)
+# -
+
+# note: we provide specific ordering here to ensure nodes are created before edges
+for path in [f"{parquet_dir}/nodes", f"{parquet_dir}/edges"]:
+
+    print(f"Working on kuzu ingest of parquet dataset: {path} ")
+    # uses wildcard functionality for all files under parquet dataset dir
+    # see: https://kuzudb.com/docusaurus/data-import/csv-import#copy-from-multiple-csv-files-to-a-single-table
+    kz_conn.execute(f'COPY {pathlib.Path(path).name} FROM "{path}/*.parquet"')
